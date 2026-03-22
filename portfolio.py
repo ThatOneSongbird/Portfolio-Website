8fa2bf1a25441a1c08bd19bb25a99c2687255db0
Ian Johnson <tempeststorms91@gmail.com>
2026-03-22 04:18:41 -0500

    updated main page, removed social media banner, updated wording
    main page now has banner, made by hero block, and updated css accordingly, as well as removed the social media box. Moved LinkedIn/Github links to About Me, and updated commenting for future viewings

--- portfolio.py
+++ portfolio.py
@@ -4,19 +4,20 @@ app = Flask(__name__)
 
 projects = [
     {
-        'title': 'Dayelis Discord TTRPG Bot',
+        'title': 'Dayelis - A Discord Bot for my Tabletop Group',
         'description': 'A Discord bot designed and maintained by me, built using Python and discord.py, to help manage the server and provide information regarding our game sessions and game system.',
         'link': 'https://github.com/ThatOneSongbird/Dayelis',
         'image': 'images/dayelis.png'
     },
     {
-        'title': 'Personal Portfolio Website',
+        'title': 'That One Portfolio - My Personal Portfolio Website',
         'description': 'This very website! Built using Flask and Python, it serves as a showcase of my projects and skills.',
-        'link': 'https://github.com/ThatOneSongbird/Portfolio-Website'
+        'link': 'https://github.com/ThatOneSongbird/Portfolio-Website',
+        'image': 'images/portfolio.png'
     },
     {
-        'title': 'Django TTRPG Blog',
-        'description': 'A blog built using Django to share my thoughts and experiences with tabletop role-playing games, as well as a place to roleplay blogging posts that are in-universe for my TTRPG campaign setting.',
+        'title': 'Dossier - A Blog focused on Tabletop Role-Playing Games and Roleplay Blogging',
+        'description': 'A blog built using Django to share my thoughts and experiences with tabletop role-playing games, as well as a place to make roleplay blogging posts that are in-universe for my TTRPG campaign setting.',
         'link': ''
     }
 ]
@@ -30,4 +31,4 @@ def home():
 def about():
     return render_template('about.html', title='About Me')
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
